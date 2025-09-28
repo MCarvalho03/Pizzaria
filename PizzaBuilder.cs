a994ce7180149cee8dfbc20ccc968692b4822856
DouglasSPinheiro <douglas.ss.pinheiro@gmail.com>
2025-09-28 13:37:31 -0300

    Update PizzaBuilder.cs
    Propostas de melhoria

--- PizzaBuilder.cs
+++ PizzaBuilder.cs
@@ -9,7 +9,27 @@ namespace Pizzaria
     public class PizzaBuilder: IBuilder
     {
         private Pizza _pizza = new Pizza();
-
+        ///*
+            Coisas para melhorar:
+                * Métodos devem retornar PizzaBuilder ou seja ( return this) assim voce pode encadear uma sequencia de chamadas
+                * Objeto do tipo pizza so deve ser criado quando for chamado o método build que ce chamou de getPizza (em teoria)
+                    na prática você cria uma pizza quando o PizzaBuilder for criado e sempre que o método build for chamado voce vai
+                        - Salvar _pizza em uma outra variavel auxiliar
+                        - criar uma nova pizza dentro de builder, assim quando o builder for chamado novamente voce não ta alterando os valores da pizza anterior
+                        - retorna a pizza que está na variavel auxiliar
+                * Outra coisa que eu faria usaria uma lista de sabores em vez de ter 3 fixos, assim voce consegue ter mais flexibilidade no pedido e o setTaste se torna o addTaste
+                * O uso seria da seguinte forma 
+                    ex: PizzaBuilder builder = new();
+                    builder.SetEdge("catupiry")
+                           .SetSize("Média")
+                           .AddTaste("catupiry")
+                           .AddTaste("Milho")
+                           .AddTaste("Soja")
+                           .Build();
+              
+               
+        *///
+        
         public void Reset()
         {
             _pizza = new Pizza();
